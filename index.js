5f1da34c28b12a7e3f857d8456073d58a9a5233b
Alexander Engström <niffik@gmail.com>
2018-12-01 21:26:43 +0100

    feat: parsing payloads

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // Dependencies
 const http = require('http')
 const url = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
@@ -26,11 +27,23 @@ const server = http.createServer((req, res) => {
   // Get the headers as an object
   const headers = req.headers
 
-  // Send the response
-  res.end('Hello World\n')
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8')
+  let buffer = ''
 
-  // Log the headers in the request
-  console.log(`Request received with these headers`, headers)
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  })
+
+  req.on('end', () => {
+    buffer += decoder.end()
+
+    // Send the response
+    res.end('Hello World\n')
+
+    // Log the headers in the request
+    console.log('Request received with this payload', buffer)
+  })
 })
 
 // Start the server, have it listen on port 3000
